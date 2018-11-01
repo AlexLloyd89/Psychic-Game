66bc28d183ebde058da7440072d569b0070a79a8
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2018-11-01 13:29:05 -0400

    Commit 11/1/2018

---
+++ script.js
@@ -0,0 +1,51 @@
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+var wins = 0;
+var losses = 0;
+var lives = 10;
+
+
+var directionsText = document.getElementById("directions-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text")
+var userInputText = document.getElementById("userInput-text");
+var livesText = document.getElementById("lives-text");
+
+
+
+//Random letter generation
+var computerOutput = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+
+//Start user input
+document.onkeyup = function (event) {
+    var userInput = event.key;
+
+
+    if (userInput === computerOutput) {
+        wins++;
+        lives = 10;
+    }
+    else {
+        lives--;
+    }
+
+    if (lives === 0) {
+        losses++;
+        lives = 10;
+    }
+
+   
+    
+
+
+//Begin text content
+directionsText.textContent = "";
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+userInputText.textContent = "Letters guessed: " + userInput;
+livesText.textContent = "Lives left: " + lives;
+};
+

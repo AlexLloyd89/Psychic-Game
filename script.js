51301926849384f0442bc7446d7b7d1ce7b0fae4
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2018-11-04 21:03:34 -0500

    11/4/2018
    Title screen now totally hidden one user selects and letter key

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var pastGuess = [];
+
 
 var wins = 0;
 var losses = 0;
@@ -26,32 +28,45 @@ document.onkeyup = function (event) {
 
 var userInput = event.key;
 
+
+
+
 for (var i = 0; i < userGuess.length; i++){
 
      if (userInput === userGuess[i]) {
 
+    document.getElementById('mydiv').classList.add("hideme"); 
+
     if (userInput.toLowerCase() === computerOutput) {
         wins++;
         lives = 10;
+        pastGuess = [];
+        computerOutput = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+        
+
     }
     else {
         lives--;
+        pastGuess.push(userInput);
     }
 
     if (lives === 0) {
         losses++;
         lives = 10;
+        pastGuess = [];
+        computerOutput = computerChoice[Math.floor(Math.random() * computerChoice.length)];
     }
 
+  
    
     
 
 
 //Begin text content
-directionsText.textContent = "";
+
 winsText.textContent = "Wins: " + wins;
 lossesText.textContent = "Losses: " + losses;
-userInputText.textContent = "Letters guessed: " + userInput;
+userInputText.textContent = "Letters guessed: " + pastGuess;
 livesText.textContent = "Lives left: " + lives;
 }
 }
